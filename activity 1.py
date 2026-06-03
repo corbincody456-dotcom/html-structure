c1e31e3d2e721b64db1db130be9a562cd2e82272
corbincody456-dotcom <corbincody456@gmail.com>
2026-06-03 20:14:01 +0800

    Add files via upload

---
+++ activity 1.py
@@ -0,0 +1,35 @@
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 7:30 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8:45 AM.")
+
+# Step 3: Playing after school
+print("I play with my classmates during recess.")
+
+# Step 4: Studying
+print("I study for 1 hour and 45 mins at night.")
+
+print("Total study hours in a week:", 2 * 7)
+
+#Printing the title
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 7:30 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8:45 AM.")
+
+# Step 3: Playing after school
+print("I play with my classmates during recess.")
+
+# Step 4: Studying
+print("I study for 1 hour and 45 mins at night.")
+
+print("Total study hours in a week:", 2 * 7)
+
+#Calculating total study time in a week
+print("Total study hours in a week:",2 * 7)
