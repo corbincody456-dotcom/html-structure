15d362bce482f03bd0e32a3fdaabb92ca39d112c
corbincody456-dotcom <corbincody456@gmail.com>
2026-07-08 20:24:47 +0800

    Add files via upload

--- Activity 37.py
+++ Activity 37.py
@@ -1,12 +1,14 @@
-def print_mirrored_triangle(rows):
-    for i in range(1, rows + 1):
-        # Print spaces
-        for j in range(rows - i):
-            print(" ", end="")
-        # Print stars
-        for k in range(i):
-            print("*", end="")
-        print() # Move to the next line
+import random 
 
-# Test the function with 5 rows
-print_mirrored_triangle(5)
+secret_number = random.randint(1, 25)
+print("I am thanking of a number bet 1 and 25.")
+
+while True:
+    guess = int(input("Take a guess: "))
+
+    if guess < secret_number: 
+        print("Too slow - - - Try again.")
+    elif guess > secret_number:
+        print("Too high - - - Try again.")
+    else:
+        print(f"Correct you found it in )
