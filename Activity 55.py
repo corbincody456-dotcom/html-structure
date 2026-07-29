8cd97ebddd1262111cdd903d12eb2ea719efe768
corbincody456-dotcom <corbincody456@gmail.com>
2026-07-29 20:09:58 +0800

    Add files via upload

---
+++ Activity 55.py
@@ -0,0 +1,9 @@
+def add(a, b): return a + b
+def subtract(a, b): return a - b
+def multiply(a, b): return a * b
+def divide(a, b): return a / b
+
+print(add(2, 3)) #5
+print(subtract(25, 5)) #20
+print(multiply(6, 4)) #24
+print(divide(20, 2)) #10
