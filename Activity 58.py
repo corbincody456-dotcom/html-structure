19557c831c9ee5e71826c53e572055347be74bbf
corbincody456-dotcom <corbincody456@gmail.com>
2026-07-31 20:07:10 +0800

    Add files via upload

---
+++ Activity 58.py
@@ -0,0 +1,15 @@
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
+
+    print("List of words with first and list characters same\n ", lst)
+    return ctr
+
+
+count = match_words(['abc', 'cfc', 'xyz', 'aba', '1221'])
+print("Numbers of words having first and list characters same:", count)
+      
